7f7cfb68b06b7e8d80352444c62a706725732fa1
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-20 13:04:31 +0530

    added registration api endpoints

--- chat/urls.py
+++ chat/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core.views import RegisterAPI
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
+    path('register/', RegisterAPI.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
